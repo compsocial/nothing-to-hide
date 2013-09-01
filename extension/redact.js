7c5a85d790029b3c6a175ad2eea4aab844da673b
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2013-09-01 18:59:15 -0400

    Process text with back-end server.

--- extension/redact.js
+++ extension/redact.js
@@ -8,27 +8,40 @@ punct = /(.*?)[.,!?:;/]$/g;
 // config based on options
 replacement = "+";
 if (options.redactionStyle == "blackout")
-  replacement = "<span style='background-color: black; color: black;'>[redacted]</span>";
+    replacement = "<span style='background-color: black; color: black;'>[redacted]</span>";
 
 lookup = top10k;
 if (options.distCutoff == "5k")
-  lookup = top5k;
+    lookup = top5k;
 else if (options.distCutoff == "1k")
-  lookup = top1k;
+    lookup = top1k;
 
-ret = "";
-for (var i=0; i<tokens.length; i++) {
-  t = tokens[i].toLowerCase();
-  match = punct.exec(t);
-  if (match != null) t = match[1];
-  if (lookup[t])
-    ret += tokens[i];
-  else {
-    if (options.redactEntire == "yes")
-      ret += replacement;
-    else
-      ret += tokens[i].substring(0,1) + replacement;
-  }
-  ret += " ";
+//var processed = commonWords(tokens, lookup, replacement);
+var processed = compose.text();
+var abstractifyAPI = "http://127.0.0.1:5000/abstractify";
+
+$.post(abstractifyAPI, {text: processed},
+       function(data) {
+           compose.html(data);
+       }, "text");
+
+function commonWords(tokens, lookup, replacement) {
+    ret = "";
+
+    for (var i=0; i<tokens.length; i++) {
+        t = tokens[i].toLowerCase();
+        match = punct.exec(t);
+        if (match != null) t = match[1];
+        if (lookup[t])
+            ret += tokens[i];
+        else {
+            if (options.redactEntire == "yes")
+                ret += replacement;
+            else
+                ret += tokens[i].substring(0,1) + replacement;
+        }
+        ret += " ";
+    }
+
+    return ret;
 }
-compose.html(ret);
