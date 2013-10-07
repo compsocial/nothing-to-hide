b3dab910e91dc70f90946507ed2854bc58e091dc
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2013-10-07 14:56:48 -0400

    Handles multiple compose windows.

--- chrome-extension/redact.js
+++ chrome-extension/redact.js
@@ -1,9 +1,5 @@
 // Show the selected options
 console.log(options);
-// Get message element
-var compose = $('div[aria-label="Message Body"]');
-// Show original message
-console.log("original: " + compose.text);
 
 // config based on options
 var replacement = "+";
@@ -16,33 +12,34 @@ if (options.distCutoff == "5k")
     lookup = top5k;
 else if (options.distCutoff == "1k")
     lookup = top1k;
-
-var message_text = compose.text();
-var html_message = compose.html();
 var abstractifyAPI = "http://127.0.0.1:5000/abstractify";
 
-var trimmed = compose.text().replace(/^\s+|\s+$/g, '');
-var tokens = trimmed.split(' ');
-var localTransforms = commonWords(tokens, lookup, replacement);
 
-// Process message with abstractify server
-$.post(abstractifyAPI, {text: message_text},
-       function(data) {
-          var serverTransforms = data;
-          var transforms = $.extend(true, {}, serverTransforms, localTransforms);
-          console.log(serverTransforms);
-          console.log(localTransforms);
-          console.log(transforms);
-          finish(transforms, html_message);
-       }, "json");
+$('div[aria-label="Message Body"]').each(function (i, compose_element) {
+    // Convert into Jquery object
+    var jcompose_element = $(compose_element);
+    var message_text = jcompose_element.text();
+    var trimmed = message_text.replace(/^\s+|\s+$/g, '');
+    var tokens = trimmed.split(' ');
+    var localTransforms = commonWords(tokens, lookup, replacement);
 
-function finish(transformations, html_message) {
-    var processed = html_message;
-    $.each(transformations, function(i, val) {
-        processed = processed.replace(i, val);
+    // Process message with abstractify server
+    $.post(abstractifyAPI, {text: message_text},
+           function(data) {
+               var serverTransforms = data;
+               var transforms = $.extend(true, {}, serverTransforms,
+                                         localTransforms);
+               finish(serverTransforms, jcompose_element);
+           }, "json");
+});
+
+function finish(transformations, compose_element) {
+    var processed = compose_element.html();
+    $.each(transformations, function(regularWord, vagueWord) {
+        processed = processed.replace(regularWord, vagueWord);
     });
 
-    compose.html(processed);
+    compose_element.html(processed);
 }
 
 function commonWords(tokens, lookup, replacement) {
@@ -56,7 +53,7 @@ function commonWords(tokens, lookup, replacement) {
             if (options.redactEntire == "yes")
                 transformations[tokens[i]] = replacement;
             else
-                transformations[tokens[i]] = tokens[i].substring(0,3)
+                transformations[tokens[i]] = tokens[i].substring(0,2)
                 + replacement;
         }
     }
