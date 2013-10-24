55777e7f9e5be02a38ef276ccbe7d9e52fc5de01
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2013-10-24 14:01:48 -0400

    Adds new run script.

---
+++ utils/run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Start the NER server
+NER_DIR="abstractor-server/stanford-ner-2013-04-04"
+NER_JAR="stanford-ner.jar"
+
+# Move to NER_DIR
+cd "$NER_DIR"
+
+# Run the NER server in the background
+java -mx1000m -cp "$NER_JAR" edu.stanford.nlp.ie.NERServer \
+    -loadClassifier "classifiers/english.all.3class.distsim.crf.ser.gz" \
+    -port 9000 -outputFormat inlineXML &
+
+# Start the Python Flask server
+python ../abstractor.py
