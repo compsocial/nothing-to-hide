c1d6ef083cdadd4b0e35e115a3d83d98baf1a9ad
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2013-10-27 18:45:10 -0400

    Makes server easier to stop.

--- utils/run.sh
+++ utils/run.sh
@@ -2,7 +2,7 @@
 
 # Start the NER server
 NER_DIR="abstractor-server/stanford-ner-2013-04-04"
-NER_JAR="stanford-ner.jar"
+NER_JAR="stanford-ner-2013-04-04.jar"
 
 # Move to NER_DIR
 cd "$NER_DIR"
@@ -13,4 +13,6 @@ java -mx1000m -cp "$NER_JAR" edu.stanford.nlp.ie.NERServer \
     -port 9000 -outputFormat inlineXML &
 
 # Start the Python Flask server
-python ../abstractor.py
+python ../abstractor.py &
+
+trap 'kill $(jobs -p)' SIGINT
