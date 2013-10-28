f716867e8545353a4299bb28d7cfbff3df9ca341
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2013-10-28 17:01:16 -0400

    Stopping the run script now works.

--- utils/run.sh
+++ utils/run.sh
@@ -1,26 +1,33 @@
 #!/bin/sh
 
+kill_em_all() {
+    echo "(CTRL+C was hit)"
+    kill 0
+    exit 0
+}
+
+trap 'kill_em_all' INT
+
 # Start the NER server
 NER_DIR="abstractor-server/stanford-ner-2013-04-04"
 FLASK_DIR="abstractor-server/abstractor"
 NER_JAR="stanford-ner-2013-04-04.jar"
-
-trap 'jobs -p | xargs kill' EXIT
 
 cd "$NER_DIR" # Move to NER_DIR
 
 # Run the NER server in the background
 java -mx1000m -cp "$NER_JAR" edu.stanford.nlp.ie.NERServer \
     -loadClassifier "classifiers/english.all.3class.distsim.crf.ser.gz" \
-    -port 9000 -outputFormat inlineXML &
+    -port 9000 -outputFormat inlineXML > /dev/null 2>&1 &
 
 cd - # Return to orginal dir
 cd "$FLASK_DIR" # Move to Flask Python server directory
 
 # Start the Python Flask server
-python abstractor.py &
+python abstractor.py > /dev/null 2>&1 &
 
-# other part of the solution:
-while pgrep -P "$BASHPID" > /dev/null; do
-    wait
+while :
+do
+    echo "Press [CTRL+C] to stop.."
+    sleep 1
 done
