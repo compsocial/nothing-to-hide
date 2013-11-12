86ca63f512c0bfc4e5eb0b0fd8b844b38b7134d5
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2013-11-12 14:38:56 -0500

    Open options page when clicking on extension icon.

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,5 +1,10 @@
 "use strict";
 
+// Open options page when clicking extension icon
+chrome.browserAction.onClicked.addListener(function() {
+    chrome.tabs.create({url: "options.html"});
+});
+
 // Fetch options and send them to content script
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
